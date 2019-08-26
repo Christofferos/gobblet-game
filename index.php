8fe571a05f28eb68129400eb3fe2fe18fa077d94
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-08-26 23:59:48 +0200

    Final Version of Gobblet game to Github, can be found on Foxdrop website

---
+++ index.php
@@ -0,0 +1,77 @@
+<link rel="shortcut icon" type="image/png" href="../../images/firefoxLogo.png">
+<section class="main-container">
+	<div class="main-wrapper">
+		<!DOCTYPE html>
+			<html>
+				<head>
+					<meta charset="utf-8" />
+					<meta http-equiv="X-UA-Compatible"
+					content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+					<!––<link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+				    <title>Gobblet</title>
+
+				    <style>
+				    	body {
+				    		display: flex;
+				    		align-items: center;
+				    		justify-content: center;
+				    		background: #202020;
+                        }
+                        img[src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] {
+						  display: none !important;
+						}
+				    </style>
+
+				    <script
+					  src="https://code.jquery.com/jquery-3.3.1.slim.js"
+					  integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+					  crossorigin="anonymous"></script>
+				</head>
+                                
+				<body bgcolor="#202020">
+					<header class="bg-dark text-center text-white p-3 mb-5">
+						<h1>Gobblet</h1>
+					</header>
+
+					<h1 style="color:#FFFFFF; text-align:center">Game for 2 players</h1>
+					<br>
+                    <div style="text-align:center; width: 10em; margin:0px auto;">
+                        <form class="difficulty-form" action="game.html">
+                            <button type="submit" class="btn-success btn-lg btn-block" name="mode" value="local">Play Local <br>1 vs. 1</button>
+                        </form>
+                    </div>
+
+					 <!-- Instructions -->
+                     <div class="row mt-5">
+                        <div class="col-md-12">
+                            <div class="card card-body bg-dark text-white">
+	                            <h5 style="text-align:center">Gobblet Rules:</h5>
+	                            <br>
+	                            <p style="text-align:center">
+	                            	Goal of the Game: <br>The first player to build a <b>horizontal</b>, <b>vertical</b> or <b>diagonal</b> line of 4 pieces is the winner!
+	                            	<br>
+	                            	<br>
+	                            	Playing pieces (Gobblets) in sizes of four: <br>3 big, 3 medium, 3 small and 3 mini. 
+	                            	<br><br>
+	                            	In turns, players can either: <br>
+	                            	1. Put a new Gobblet on an empty square.<br>
+	                            	2. Pick up a Gobblet (of your color) already on the playing field and place it over a smaller Gobblet.
+	                            	<br>
+	                            	<br> 
+	                            	Clarification/Exceptions: <br> 1. If your opponent already has 3 in a row, you may take a Gobblet <b>from an external stack and place it over 1 of the 3 pieces in the line</b>.<br> 2. If you uncover one of your opponent’s pieces, and this move reveals a line of 4 of your opponent’s pieces, <br>you lose unless you can place your gobblet over a different piece in this same line of 4.
+	                            	<br>
+	                            	<br>
+	                            	Link to rules: <br>(If you want to have it on a seperate tab while playing)<br>
+	                            	<a href="https://www.boardspace.net/gobblet/english/gobblet_rules.pdf" target="_blank">PDF FILE for Gobblet rules</a>
+								</p>
+                            </div>
+                        </div>
+                    </div>
+
+				</body>
+			</html>
+	</div>
+</section>
